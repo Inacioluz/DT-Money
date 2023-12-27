58719d3ed4e1fd5695e06d13bf0793bc46e7e7c6
Inacio Silva <joseinaciolds@gmail.com>
2023-12-26 21:22:28 -0300

    tabela de transações ok

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles";
 
 export function Transactions() {
-    return(
+    return (
         <div>
-            <Header/>
-            <Summary/>
+            <Header />
+            <Summary />
+
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00    
+                                </PriceHighlight>
+                            </td>
+                            <td>Vendas</td>
+                            <td>13/07/2015</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Alimnetação</td>
+                            <td>
+                                <PriceHighlight variant="outline">
+                                    - R$ 60,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Vendas</td>
+                            <td>15/03/2021</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 
