8b2c78c6ffd525b42d6cf5b812be1cfdd194c351
Inacio Silva <joseinaciolds@gmail.com>
2023-12-27 07:04:53 -0300

    SearchForm ok

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles";
 
 export function Transactions() {
@@ -10,13 +11,14 @@ export function Transactions() {
 
 
             <TransactionsContainer>
+                <SearchForm />
                 <TransactionsTable>
                     <tbody>
                         <tr>
                             <td width="50%">Hamburguer</td>
                             <td>
                                 <PriceHighlight variant="income">
-                                    R$ 12.000,00    
+                                    R$ 12.000,00
                                 </PriceHighlight>
                             </td>
                             <td>Vendas</td>
