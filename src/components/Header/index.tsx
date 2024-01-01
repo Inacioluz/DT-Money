148de1c6d54fd5dafe70b764f67cd61982433946
Inacio Silva <joseinaciolds@gmail.com>
2024-01-01 18:19:08 -0300

    Criando modal acessivel

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransationButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/money-logo.svg'
 
 export function Header() {
     return (
-            <HeaderContainer>
-                <HeaderContent>
-                    <img src={logoImg} />
-                    
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} />
 
-                    <NewTransationButton>Nova Transação</NewTransationButton>
-                </HeaderContent>
-            </HeaderContainer>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransationButton>Nova Transação</NewTransationButton>
+                    </Dialog.Trigger>
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        
+                    </Dialog.Portal>
+                    
+                    <Dialog.Content>
+                        <Dialog.Title>Nova Transação</Dialog.Title>
+                        <Dialog.Close />
+                    </Dialog.Content>
+
+                </Dialog.Root>
+            </HeaderContent>
+        </HeaderContainer>
     )
 }
